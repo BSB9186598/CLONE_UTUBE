82f6def16fa1b0f2e56c12db042b2a44165f6784
BSB9186598 <bhaskarbhandari372@gmail.com>
2023-11-28 23:26:06 +0530

    jacdguae

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,7 +12,7 @@ export const UTUBE_VIDEO_API =
 "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   GOOGLE_API_KEY;
 
-export const UTUBE_SEARCH_API= "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=" 
+export const UTUBE_SEARCH_API= "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=" 
 
 export const USER_IMG = "https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
 
